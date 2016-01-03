71ff128db3d8cc0df9ced59737d5619765e2714f
Jordan Jozwiak <jordanjoz1@gmail.com>
2016-01-03 02:23:15 -0800

    cleanup routes

--- main.go
+++ main.go
@@ -16,69 +16,49 @@ func main() {
 
 	db, err := gorm.Open("postgres", "password=mysecretpassword host=localhost port=5432 sslmode=disable")
 	if err != nil {
-		log.Fatal("Databas open error. Error:", err.Error())
+		log.Fatal("Unable to open database:", err.Error())
 	}
-	log.Println("Database openned!")
 	err = db.DB().Ping()
 	if err != nil {
-		log.Fatal("Database ping error! Error:", err.Error())
+		log.Fatal("Unable to ping database:", err.Error())
 	}
 
 	db.DB().SetMaxIdleConns(10)
 	db.DB().SetMaxOpenConns(100)
 
-	// migrations
+	// run migrations
 	db.AutoMigrate(&models.Post{}, &models.Group{}, &models.User{}, &models.Vote{}, &models.Comment{})
 
+	// get controller instances
+	pc := controllers.NewPostController(db)
+	gc := controllers.NewGroupController(db)
+	cc := controllers.NewCommentController(db)
+	uc := controllers.NewUserController(db)
+
+	// init router
 	router := gin.Default()
 
 	// session management
 	store := sessions.NewCookieStore([]byte("secret")) //TODO use environment variable secret
 	router.Use(sessions.Sessions("ddvote_session", store))
 
-	authorized := router.Group("/")
-
-	authorized.Use(UseAuth)
-
-	authorized.GET("/incr", func(c *gin.Context) {
-		session := sessions.Default(c)
-		var count int
-		v := session.Get("count")
-		if v == nil {
-			count = 0
-		} else {
-			count = v.(int)
-			count++
-		}
-		session.Set("count", count)
-		session.Save()
-		c.JSON(200, gin.H{"count": count})
-	})
-
-	// Simple group: v1
+	// api v1 calls WITHOUT auth
 	v1 := router.Group("api/v1")
 	{
-		v1.GET("/ping", func(c *gin.Context) {
-			session := sessions.Default(c)
-			session.Set("uid", 123)
-			session.Save()
-			c.String(200, "pong")
-		})
-
-		// get controller instances
-		pc := controllers.NewPostController(db)
-		gc := controllers.NewGroupController(db)
-		cc := controllers.NewCommentController(db)
-		uc := controllers.NewUserController(db)
-
 		v1.POST("/login", uc.LoginWithClientID)
 		v1.POST("/logout", uc.Logout)
-		v1.GET("/post", pc.GetAllPostsForGroup)
-		v1.POST("/post", pc.CreatePost)
-		v1.POST("/group", gc.GetOrCreateGroup)
-		v1.POST("/comment", cc.CreateComment)
-
 	}
+
+	// api v1 calls WITH auth
+	v1auth := router.Group("api/v1")
+	{
+		v1auth.Use(UseAuth)
+		v1auth.GET("/post", pc.GetAllPostsForGroup)
+		v1auth.POST("/post", pc.CreatePost)
+		v1auth.POST("/group", gc.GetOrCreateGroup)
+		v1auth.POST("/comment", cc.CreateComment)
+	}
+
 	router.Run(":8080")
 }
 
@@ -86,7 +66,7 @@ func UseAuth(c *gin.Context) {
 	session := sessions.Default(c)
 	v := session.Get("uid")
 	if v == nil {
-		c.String(401, "User is not logged in")
+		c.JSON(401, models.ApiResponse{IsError: false, Message: "User is not logged in"})
 		c.Abort()
 	}
 }
