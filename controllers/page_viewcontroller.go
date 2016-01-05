62b149b0bd6010fc2a02c81944f9246c18fdfc48
Jordan Jozwiak <jordanjoz1@gmail.com>
2016-01-04 20:45:56 -0800

    create new group if one does not already exist

--- controllers/page_viewcontroller.go
+++ controllers/page_viewcontroller.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/jinzhu/gorm"
+	"github.com/jordanjoz/dd-vote/models"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,5 +20,11 @@ func NewPageViewController(db gorm.DB) *PageViewController {
 }
 
 func (gc PageViewController) ShowGroupPage(c *gin.Context) {
+	// create new group if it does not exist
+	gid := c.Param("gid")
+	var group models.Group
+	gc.db.FirstOrCreate(&group, models.Group{Name: gid})
+
+	// show view
 	http.ServeFile(c.Writer, c.Request, "views/group_page.html")
 }
